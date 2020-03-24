757fc2dc89018cb27911b245d0a2ff9210b2668d
gaba <61543034+gabavip@users.noreply.github.com>
2020-03-24 14:33:30 +0800

    Update

---
+++ precache-manifest.cbfe846de8c0e410e681ab1a79c23ef3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abdaea9c0e9a6e733097",
+    "url": "/css/app.18df9375.css"
+  },
+  {
+    "revision": "e2667c7d66914a40ba64",
+    "url": "/css/chunk-vendors.55a32a6c.css"
+  },
+  {
+    "revision": "d1fc85ca7e2508adfb9424235d0f91ff",
+    "url": "/fonts/iconfont.d1fc85ca.ttf"
+  },
+  {
+    "revision": "da442d3d43ce8bf7dd2ffc1f3c4addca",
+    "url": "/img/CrowdfundingBg.da442d3d.png"
+  },
+  {
+    "revision": "9a522ce26501f2586d586f13c586bb0c",
+    "url": "/img/LuckyBox.9a522ce2.png"
+  },
+  {
+    "revision": "764c617f007114da15f98f278868b9ac",
+    "url": "/img/My-Logo.764c617f.png"
+  },
+  {
+    "revision": "822ccdd66bf4f58243b5e4a753fc06cf",
+    "url": "/img/StatisticsBoxBottomLeft.822ccdd6.png"
+  },
+  {
+    "revision": "f3c944b59f7129fdd60f1c7621b61ae0",
+    "url": "/img/StatisticsBoxBottomRight.f3c944b5.png"
+  },
+  {
+    "revision": "c423cd020b8e6b9322f94deb02c1f3d6",
+    "url": "/img/StatisticsBoxTopLeft.c423cd02.png"
+  },
+  {
+    "revision": "f8d40d38085d116e0b53e492fbe5a559",
+    "url": "/img/StatisticsBoxTopRight.f8d40d38.png"
+  },
+  {
+    "revision": "20e8e654c84458760dbddf3b69fbad4e",
+    "url": "/img/bg.20e8e654.png"
+  },
+  {
+    "revision": "20e8e654c84458760dbddf3b69fbad4e",
+    "url": "/img/bg.png"
+  },
+  {
+    "revision": "32c026d71516565ab18c984be357ed07",
+    "url": "/img/changeTopBox.32c026d7.png"
+  },
+  {
+    "revision": "540770dacd24c2f9938e2c23e0b5f5bb",
+    "url": "/img/incomeTopBg.540770da.png"
+  },
+  {
+    "revision": "df3cb110ae04ed05458b8fdaa965b91c",
+    "url": "/img/limit-bg.df3cb110.png"
+  },
+  {
+    "revision": "3cfd82079a166a0c0b5c612ff68e6828",
+    "url": "/img/myCodeBg.3cfd8207.png"
+  },
+  {
+    "revision": "faf69b9355915803cbfc6dcbe0073d80",
+    "url": "/img/yellowRing.faf69b93.png"
+  },
+  {
+    "revision": "4d269b3c4a993292e683bc44cac31296",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abdaea9c0e9a6e733097",
+    "url": "/js/app.99b7042d.js"
+  },
+  {
+    "revision": "e2667c7d66914a40ba64",
+    "url": "/js/chunk-vendors.fc51260e.js"
+  },
+  {
+    "revision": "3d039d53e375abb98478cdf83925ab90",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
